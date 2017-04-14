ba59d5d034036a888ecf575f92d34d23b793c30d
Nicky Semenza <nicky@nickysemenza.com>
2017-04-14 01:54:24 -0400

    image https

--- src/Home.js
+++ src/Home.js
@@ -27,7 +27,7 @@ export default class Home extends Component {
           <div className="w-100 hidden-md-up" />
           <div className="col">
             <iframe
-              src="http://www.nicky.photos/frame/slideshow?key=NrsTC5&autoStart=1&captions=1&navigation=1&playButton=0&randomize=1&speed=3&transition=fade&transitionSpeed=1"
+              src="https://www.nicky.photos/frame/slideshow?key=NrsTC5&autoStart=1&captions=1&navigation=1&playButton=0&randomize=1&speed=3&transition=fade&transitionSpeed=1"
               width="100%"
               height="600"
               frameBorder="no"
