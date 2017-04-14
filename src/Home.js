ca07b09ce5ebcc83db02000a2a04fb67b8b03d2d
Nicky Semenza <nicky@nickysemenza.com>
2017-04-14 00:33:09 -0400

    added photo gallery

--- src/Home.js
+++ src/Home.js
@@ -19,7 +19,20 @@ export default class Home extends Component {
         return (
             <div>
                 <h2>Nicky's Recipe Stash</h2>
-                {a}
+                <hr/>
+                <div className="row">
+                    <div className="col">
+                        {a}
+                    </div>
+                    <div className="w-100 hidden-md-up"></div>
+                    <div className="col">
+                        <iframe src="http://www.nicky.photos/frame/slideshow?key=NrsTC5&autoStart=1&captions=1&navigation=1&playButton=0&randomize=1&speed=3&transition=fade&transitionSpeed=1" width="100%" height="600" frameBorder="no" scrolling="no"></iframe>
+                    </div>
+                </div>
+                <hr/>
+                View this project on github: <a href="https://github.com/nickysemenza/food" target="_blank">github.com/nickysemenza/food</a>
+                <br/>
+                <a href="http://www.nicky.photos/Food/My-Food/" target="_blank">View all photos</a>
             </div>
         );
     }
