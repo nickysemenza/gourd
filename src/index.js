686d3dff07ed9636570e415ad0a3364f10240c48
Nicky Semenza <nicky@nickysemenza.com>
2017-11-29 14:16:15 -0500

    react upgrade

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
