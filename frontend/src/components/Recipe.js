b16d10566ad9c08ef6f0624830ccf5a38d8f8475
Nicky Semenza <nicky@nickysemenza.com>
2017-12-20 15:42:13 -0800

    cleaned up recipe view

--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import RecipeTable from './RecipeTable';
+import {
+  Button,
+  Divider,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Segment
+} from 'semantic-ui-react';
 export default class Recipe extends Component {
   constructor(props) {
     super(props);
@@ -18,77 +27,67 @@ export default class Recipe extends Component {
     if (!recipe) return <div>loading...</div>;
     if (recipe.error !== undefined) return <div>error! {recipe.error}</div>;
 
+    let header = (
+      <Header as="h1">
+        {recipe.title}
+        <Header.Subheader>
+          {`From ${recipe.source}. Makes ${parseFloat(
+            (recipe.quantity * this.state.scale).toFixed(1)
+          )} ${recipe.unit}`}
+        </Header.Subheader>
+      </Header>
+    );
+    let rightSidebar = (
+      <Segment>
+        <Header as="h3">Scaling</Header>
+        <p>
+          <b>Approx weight:</b>&nbsp; TODO::CALC
+        </p>
+        <p>
+          <b>Total minutes:</b>&nbsp;{recipe.totalMinutes}
+        </p>
+        <p>
+          <b>Total minutes (calculated):</b>&nbsp;TODO::CALC
+        </p>
+        <p>
+          <b>Equipment:</b>&nbsp;{recipe.equipment}
+        </p>
+        <Form>
+          <Form.Field width={4}>
+            <label>Multiplier</label>
+            <input
+              type="number"
+              min="0"
+              max="10"
+              step=".1"
+              value={this.state.scale}
+              onChange={this.handleScaleChange}
+            />
+          </Form.Field>
+        </Form>
+        <Divider />
+        <Header as="h3">Misc</Header>
+        <Button
+          as="Link"
+          to={`/editor/${this.props.slug}`}
+          content="edit recipe"
+          icon="edit"
+        />
+        <Divider />
+        <Image src="http://via.placeholder.com/2000x1200" />
+      </Segment>
+    );
     return (
       <div>
-        <h1>{recipe.title}</h1>
-        <h4>
-          From {recipe.source} . Makes{' '}
-          <i>{parseFloat((recipe.quantity * this.state.scale).toFixed(1))}</i>{' '}
-          {recipe.unit}
-        </h4>
-        <div className="row">
-          <div className="col col-sm-12">
+        {header}
+        <Grid>
+          <Grid.Column width={12}>
             <RecipeTable recipe={recipe} scale={this.state.scale} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col col-sm-6">
-            <div className="card">
-              <div className="card-block">
-                <h4 className="card-title">Scaling</h4>
-                <p className="card-text">
-                  approx weight:&nbsp; TODOCALCg
-                  <Link to={`/editor/${this.props.slug}`}>
-                    <button>edit</button>
-                  </Link>
-                </p>
-                <hr />
-                <div className="form-group row">
-                  <label htmlFor="example-text-input">Multiplier</label>
-                  <br />
-                  <input
-                    className="form-control"
-                    type="number"
-                    min="0"
-                    max="10"
-                    step=".1"
-                    value={this.state.scale}
-                    onChange={this.handleScaleChange}
-                    id="example-text-input"
-                  />
-                </div>
-              </div>
-              <hr />
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <b>Total minutes:</b>&nbsp;{recipe.totalMinutes}
-                  <b>Total minutes (calculated):</b>&nbsp;TODOCALC
-                </li>
-                <li className="list-group-item">
-                  <b>Equipment:</b>&nbsp;{recipe.equipment}
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col col-sm-6">
-            <div className="card">
-              <p className="card-text">
-                {recipe.image ? (
-                  <img
-                    src={recipe.image}
-                    alt=""
-                    style={{
-                      maxHeight: '350px',
-                      maxWidth: '350px',
-                      height: 'auto',
-                      width: 'auto'
-                    }}
-                  />
-                ) : null}
-              </p>
-            </div>
-          </div>
-        </div>
+            <Header as="h1">Notes</Header>
+            <p>TODO</p>
+          </Grid.Column>
+          <Grid.Column width={4}>{rightSidebar}</Grid.Column>
+        </Grid>
       </div>
     );
   }
