fa45b902cf267021e817f551451ba548c5778505
Nicky Semenza <nicky@nickysemenza.com>
2020-04-25 16:38:30 -0700

    cypress codecov

--- ui/cypress/plugins/index.js
+++ ui/cypress/plugins/index.js
@@ -14,4 +14,9 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  require("@cypress/code-coverage/task")(on, config);
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config;
 };
