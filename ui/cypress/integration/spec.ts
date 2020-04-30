bd74635c184e4f3a7c2e70e8e845b28a972eb341
Nicky Semenza <nicky@nickysemenza.com>
2020-04-29 18:41:59 -0700

    test cleanup

--- ui/cypress/integration/spec.ts
+++ ui/cypress/integration/spec.ts
@@ -2,15 +2,12 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
-context("Basic Create List test", () => {
+context("Basic Create, List, Edit test", () => {
   beforeEach(() => {
-    // usually we recommend setting baseUrl in cypress.json
-    // but for simplicity of this example we just use it here
-    // https://on.cypress.io/visit
     cy.visit("/");
   });
 
-  it("creates a new one", function () {
+  it("creates a new recipe", function () {
     const newName = "cy-" + new Date().getTime();
     cy.contains("Create").click();
     cy.get("input[data-cy=name-input]").first().type(newName);
@@ -18,13 +15,11 @@ context("Basic Create List test", () => {
     // cy.contains("Recipes").click();
   });
   it("updates the recipe", function () {
-    const newName = "ingredient-" + new Date().getTime();
+    const newIngredient = "ingredient-" + new Date().getTime();
     cy.contains("Recipes").click();
     cy.get("[data-cy=recipe-table]").should("be.visible");
     cy.contains("details").click();
     cy.url().should("include", "/recipe/");
-
-    // cy.get("[data-cy=recipe-name]").invoke("text").as("text");
 
     cy.contains("edit").click();
     cy.contains("add section").click();
@@ -32,16 +27,15 @@ context("Basic Create List test", () => {
     cy.get("input[data-cy=grams-input]")
       .first()
       .type("{selectall}4");
-    cy.get("input[data-cy=name-input]").first().type(newName);
+    cy.get("input[data-cy=name-input]").first().type(newIngredient);
     cy.contains("add instruction").click();
     cy.get("input[data-cy=instruction-input]")
       .first()
       .type("mix");
     cy.contains("save").click();
     cy.contains("Ingredients").click();
-    cy.contains(newName);
+    cy.contains(newIngredient);
   });
-
   // more examples
   //
   // https://github.com/cypress-io/cypress-example-todomvc
