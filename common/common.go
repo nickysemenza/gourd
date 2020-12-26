6d930b9799a778365d539d0e0f6dafac9be2a836
Nicky Semenza <nicky@nickysemenza.com>
2020-12-26 10:24:54 -0800

    shorten uuids

--- common/common.go
+++ common/common.go
@@ -1,12 +1,24 @@
 package common
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 
 	"github.com/gofrs/uuid"
 )
 
 func UUID() string {
+	if true {
+		b := make([]byte, 4)
+		_, err := rand.Read(b)
+		if err != nil {
+			panic(fmt.Errorf("failed to make uuid: %w", err))
+		}
+		s := hex.EncodeToString(b)
+		return s
+	}
+
 	u, err := uuid.NewV4()
 	if err != nil {
 		panic(fmt.Errorf("failed to make uuid: %w", err))
