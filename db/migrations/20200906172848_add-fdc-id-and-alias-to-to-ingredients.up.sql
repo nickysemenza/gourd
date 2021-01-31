e4a81e9538bfbaa01c62274bc638a50003492022
Nicky Semenza <nicky@nickysemenza.com>
2021-01-31 12:16:20 -0800

    parallelize food fetching queries

--- db/migrations/20200906172848_add-fdc-id-and-alias-to-to-ingredients.up.sql
+++ db/migrations/20200906172848_add-fdc-id-and-alias-to-to-ingredients.up.sql
@@ -2,3 +2,8 @@ ALTER TABLE "ingredients"
 ADD COLUMN "fdc_id" INT4;
 ALTER TABLE "ingredients"
 ADD COLUMN "same_as" TEXT references ingredients(id) DEFAULT NULL;
+-- https://www.alibabacloud.com/blog/postgresql-fuzzy-search-best-practices-single-word-double-word-and-multi-word-fuzzy-search-methods_595635
+-- create index idx on usda_food(description collate "C");
+-- https://scoutapm.com/blog/how-to-make-text-searches-in-postgresql-faster-with-trigram-similarity
+CREATE EXTENSION pg_trgm;
+CREATE INDEX idx2 ON usda_food USING GIN(description gin_trgm_ops);
