706faeb71a36a1d7978f17a33b282fcf69c50643
Nicky Semenza <nicky@nickysemenza.com>
2021-03-16 18:32:26 -0700

    fix tests

--- ui/cypress/integration/spec.ts
+++ ui/cypress/integration/spec.ts
@@ -38,7 +38,7 @@ context("Basic Create, List, Edit test", () => {
     cy.contains("add section").click();
     cy.contains("add ingredient").click();
     cy.get("input[data-cy=grams-input]").first().type("{selectall}4");
-    cy.get("div[data-cy=name-input]").first().type(`${newIngredient}`);
+    cy.get("div[data-cy=name-input]").find("input").type(`${newIngredient}`);
     cy.wait(500);
     cy.contains(`create ingredient: ${newIngredient}`);
 
