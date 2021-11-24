4f88a1c1674207d14b71e18e838aab95cf113f7a
Nicky Semenza <nicky@nickysemenza.com>
2021-11-23 20:00:39 -0800

    swap force graph for highcharts

--- ui/src/pages/Graph.tsx
+++ ui/src/pages/Graph.tsx
@@ -1,43 +1,103 @@
-import React from "react";
-import ForceGraph2D, { GraphData, NodeObject } from "react-force-graph-2d";
+import React, { useRef } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import HighchartsNetworkGraph from "highcharts/modules/networkgraph";
+
 import { useRecipeDependencies } from "../api/openapi-hooks/api";
 import { useHistory } from "react-router-dom";
 
-type CustomNode = NodeObject & {
+type CustomPoint = Highcharts.Point & {
   raw_kind: "ingredient" | "recipe";
   raw_id: string;
 };
+
 const Graph: React.FC = () => {
+  HighchartsNetworkGraph(Highcharts);
+
   const { data } = useRecipeDependencies({});
   let history = useHistory();
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
 
   if (!data || !data.items) return null;
-  const data3: GraphData = {
-    links: data.items.map((d) => {
-      return {
-        source: d.ingredient_name,
-        target: d.recipe_name,
-        value: d.ingredient_kind === "ingredient" ? 1 : 40,
-      };
-    }),
-    nodes: [
-      ...data.items.map((d) => ({
-        id: d.ingredient_name,
-        raw_id: d.ingredient_id,
-        raw_kind: d.ingredient_kind,
-        group: d.ingredient_kind === "ingredient" ? 1 : 4,
-      })),
-      ...data.items.map((d) => ({
-        id: d.recipe_name,
-        raw_id: d.recipe_id,
-        raw_kind: "recipe",
-        group: 30,
-      })),
-    ].filter(
-      (thing, index, self) => index === self.findIndex((t) => t.id === thing.id)
-    ),
+
+  const links = data.items.map((d) => {
+    return {
+      from: d.ingredient_name,
+      to: d.recipe_name,
+      value: d.ingredient_kind === "ingredient" ? 1 : 40,
+    };
+  });
+  const nodes = [
+    ...data.items.map((d) => ({
+      id: d.ingredient_name,
+      raw_id: d.ingredient_id,
+      raw_kind: d.ingredient_kind,
+      group: d.ingredient_kind === "ingredient" ? 1 : 4,
+      color: "orange",
+    })),
+    ...data.items.map((d) => ({
+      id: d.recipe_name,
+      raw_id: d.recipe_id,
+      raw_kind: "recipe",
+      group: 30,
+      color: "green",
+    })),
+  ].filter(
+    (thing, index, self) => index === self.findIndex((t) => t.id === thing.id)
+  );
+
+  const options: Highcharts.Options = {
+    chart: {
+      type: "networkgraph",
+    },
+    title: {
+      text: "My chart",
+    },
+    plotOptions: {
+      networkgraph: {
+        keys: ["from", "to"],
+        layoutAlgorithm: {
+          enableSimulation: true,
+          // friction: -0.9,
+          integration: "verlet",
+          linkLength: 100,
+        },
+      },
+    },
+    series: [
+      {
+        link: {
+          color: "rgba(100, 100, 100, 0.8)",
+          dashStyle: "dash",
+        },
+        point: {
+          events: {
+            click: function () {
+              var node = this as CustomPoint;
+              console.log(node);
+
+              history.push(
+                node.raw_kind === "recipe"
+                  ? `/recipe/${node.raw_id}`
+                  : `/ingredients/${node.raw_id}`
+              );
+            },
+          },
+        },
+
+        type: "networkgraph",
+        dataLabels: {
+          enabled: true,
+          linkFormat: "",
+        },
+        id: "lang-tree",
+        data: links,
+        nodes: nodes,
+      },
+    ],
   };
-  console.log(data3);
+
+  console.log({ links, nodes });
   return (
     <div>
       <div className="text-gray-900 flex">Graph</div>
@@ -45,68 +105,10 @@ const Graph: React.FC = () => {
         className="border border-indigo-600"
         style={{ width: "1000px", height: "800px" }}
       >
-        <ForceGraph2D
-          width={1000}
-          height={800}
-          // dagMode="radialout"
-          onNodeClick={(n, _e) => {
-            const node = n as CustomNode;
-            history.push(
-              node.raw_kind === "recipe"
-                ? `/recipe/${node.raw_id}`
-                : `/ingredients/${node.raw_id}`
-            );
-            console.log(node);
-          }}
-          graphData={data3}
-          nodeAutoColorBy="group"
-          // @ts-ignore
-          nodeCanvasObject={(node, ctx, globalScale) => {
-            const label = node.id || "";
-            const fontSize = 12 / globalScale;
-            ctx.font = `${fontSize}px Sans-Serif`;
-            // @ts-ignore
-            const textWidth = ctx.measureText(label).width;
-            const bckgDimensions = [textWidth, fontSize].map(
-              (n) => n + fontSize * 0.2
-            ); // some padding
-
-            ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-            // @ts-ignore
-            ctx.fillRect(
-              // @ts-ignore
-              node.x - bckgDimensions[0] / 2,
-              // @ts-ignore
-              node.y - bckgDimensions[1] / 2,
-              // @ts-ignore
-              ...bckgDimensions
-              // @ts-ignore
-            );
-
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            // @ts-ignore
-            ctx.fillStyle = node.color;
-            // @ts-ignore
-            ctx.fillText(label, node.x, node.y);
-
-            // @ts-ignore
-            node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
-          }}
-          nodePointerAreaPaint={(node, color, ctx) => {
-            ctx.fillStyle = color;
-            // @ts-ignore
-            const bckgDimensions = node.__bckgDimensions;
-            bckgDimensions &&
-              ctx.fillRect(
-                // @ts-ignore
-                node.x - bckgDimensions[0] / 2,
-                // @ts-ignore
-                node.y - bckgDimensions[1] / 2,
-                // @ts-ignore
-                ...bckgDimensions
-              );
-          }}
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+          ref={chartComponentRef}
         />
       </div>
     </div>
