938dc0b80e292c2bd1b285e6e176660a3ef2c7cf
Nicky Semenza <nicky@nickysemenza.com>
2022-10-11 19:37:05 -0700

    bump deps

--- rust/server/src/scraper.rs
+++ rust/server/src/scraper.rs
@@ -3,7 +3,7 @@ use openapi::models::{CompactRecipe, CompactRecipeMeta, CompactRecipeSection};
 
 #[tracing::instrument(name = "route::scrape_recipe")]
 pub async fn scrape_recipe(url: &str) -> Result<CompactRecipe> {
-    let s = recipe_scraper::Scraper::new();
+    let s = recipe_scraper_fetcher::Fetcher::new();
     let res = s.scrape_url(url).await?;
 
     let meta = CompactRecipeMeta {
